3ab70c306b4f7e559723cb11a529cdc9a702745a
Bryant Sarabia <bryantsarabia@gmail.com>
2020-08-10 15:28:57 +0200

    login and register

---
+++ src/main/java/com/mycompany/guida/tv/auth/Login.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.guida.tv.auth;
+
+import com.mycompany.guida.tv.controller.BaseController;
+import com.mycompany.guida.tv.data.DataException;
+import com.mycompany.guida.tv.result.FailureResult;
+import com.mycompany.guida.tv.result.TemplateManagerException;
+import com.mycompany.guida.tv.result.TemplateResult;
+import com.mycompany.guida.tv.security.SecurityLayer;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author HP
+ */
+public class Login extends BaseController {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try {
+            
+            if(request.getParameter("submit") != null ) {
+                action_login(request, response);
+            }
+            else {
+                action_default(request, response);
+            }
+        } catch (DataException | IOException | TemplateManagerException ex) {
+            request.setAttribute("exception", ex);
+            action_error(request, response);
+        }
+        
+    }
+    
+      private void action_error(HttpServletRequest request, HttpServletResponse response) {
+        if (request.getAttribute("exception") != null) {
+            (new FailureResult(getServletContext())).activate((Exception) request.getAttribute("exception"), request, response);
+        } else {
+            (new FailureResult(getServletContext())).activate((String) request.getAttribute("message"), request, response);
+        }
+    }
+      
+          
+    private void action_default(HttpServletRequest request, HttpServletResponse response) throws DataException, TemplateManagerException, IOException {
+        
+        HttpSession s = SecurityLayer.checkSession(request);
+        if(s != null) {
+            response.sendRedirect("profile");
+        }
+        else {
+   
+            TemplateResult results = new TemplateResult(getServletContext());
+            results.activate("login.ftl.html", request, response);
+        }
+        
+        
+    }
+    
+      // Effettua il login e mi redireziona sul profilo
+    private void action_login(HttpServletRequest request, HttpServletResponse response) {
+      
+        
+        /* DA SCRIVERE */
+    }
+      
+      
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
