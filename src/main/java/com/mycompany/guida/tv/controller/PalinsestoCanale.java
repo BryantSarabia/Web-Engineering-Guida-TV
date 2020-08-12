672a3f8d9e442ea4720a707ccc2d2a10d52252d5
Bryant Sarabia <bryantsarabia@gmail.com>
2020-08-12 12:51:55 +0200

    Palinsesto singolo

---
+++ src/main/java/com/mycompany/guida/tv/controller/PalinsestoCanale.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.guida.tv.controller;
+
+import com.mycompany.guida.tv.data.DataException;
+import com.mycompany.guida.tv.result.FailureResult;
+import com.mycompany.guida.tv.result.TemplateManagerException;
+import com.mycompany.guida.tv.result.TemplateResult;
+import com.mycompany.guida.tv.security.SecurityLayer;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author HP
+ */
+public class PalinsestoCanale extends BaseController {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+        response.setContentType("text/html;charset=UTF-8");
+        int fascia = 1;
+        try {
+            if (request.getParameter("fascia") != null) {
+                fascia = SecurityLayer.checkNumeric(request.getParameter("fascia"));
+                if (fascia >= 1 && fascia <= 4) {
+                    // Se la fascia non è valida la lascio a 1 (Mattina) altrimenti eseguo la action get_by_fascia
+                    action_get_by_fascia(request, response, fascia);
+                }
+            } else {
+                action_default(request, response);
+            }
+        } catch (TemplateManagerException ex) {
+            request.setAttribute("exception", ex);
+            action_error(request, response);
+
+        } catch (DataException ex) {
+            Logger.getLogger(PalinsestoCanale.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+      private void action_error(HttpServletRequest request, HttpServletResponse response) {
+        if (request.getAttribute("exception") != null) {
+            (new FailureResult(getServletContext())).activate((Exception) request.getAttribute("exception"), request, response);
+        } else {
+            (new FailureResult(getServletContext())).activate((String) request.getAttribute("message"), request, response);
+        }
+        return;
+    }
+    
+      
+      private void action_default(HttpServletRequest request, HttpServletResponse response) throws DataException, TemplateManagerException {
+        
+        action_get_by_fascia(request, response, 1);
+        
+    }
+      
+      private void action_get_by_fascia(HttpServletRequest request, HttpServletResponse response, int fascia) throws TemplateManagerException, DataException {
+        /* Da fare */
+        TemplateResult results = new TemplateResult(getServletContext());
+        results.activate("palinsesto_canale.ftl.html", request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
